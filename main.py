6bd3ef284360b9131b265013f588aed374d5f5b2
Vien Hoang <vieninsweden@gmail.com>
2025-10-18 18:11:05 +0200

    fix the votes list, now it has randomized total votes

--- main.py
+++ main.py
@@ -9,26 +9,45 @@ while not done_adding:
     # Capitalize the input name 
     candidates.append(name.capitalize())
 
-    # Add the user for more suggestions
-    add_more = str(input("Add more candidate? Yes/No "))
+    # If only 1 candidate entered
+    if len(candidates) == 1:
+        # Add the user for more suggestions
+        add_more = str(input("Add more candidate? Yes/No "))
 
-    # If no, randomize votes and save to file
-    if add_more.strip().lower() in ("no", "n") and len(candidates) > 1:
+        # No more candidates, exit program
+        if add_more.strip().lower() in ("no", "n"):
 
-        # Enter how many votes that shoud be casted
-        nbr_votes = int(input("How many votes should the program randomize? "))
+            print(f"Only one candidate. The class president is {candidates[0]}. No need for democracy.")
+            done_adding = True
 
-        # Loop through the number of votes, add 1 vote randomally in each votes element
-        # Enter the total votes in the class
-        for r in range(0, nbr_votes):
-            vote = 1
-            votes_element = random.randint(0, len(candidates)) 
-            votes[votes_element] += vote
+    else:
 
-        print("Inside no")
-        done_adding = True
+        # Add the user for more suggestions
+        add_more = str(input("Add more candidate? Yes/No "))
 
-        # Save the cast votes to file
+        # If no, randomize votes and save to file
+        if add_more.strip().lower() in ("no", "n"):
+
+            # Set the votes list to 0 for all the candidates
+            for c in candidates:
+                votes.append(0)
+
+            # Enter how many votes that shoud be casted
+            nbr_votes = int(input("How many votes should the program randomize? "))
+
+            # Loop through the number of votes, add 1 vote randomally in each votes element
+            # Enter the total votes in the class
+            for r in range(0, nbr_votes):
+                vote = 1
+                # Randomize a votes element so the vote can be stored
+                votes_element = random.randint(0, len(candidates) - 1)
+                # Add one vote to a randomized votes element
+                votes[votes_element] += vote
+
+            print("Inside no")
+            done_adding = True
+
+            # Save the candidates and votes to file
 
 
     print(candidates, votes)
