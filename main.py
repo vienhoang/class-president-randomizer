e459d85f3eceda47c90778e91710714bd7033608
Vien Hoang <vieninsweden@gmail.com>
2025-10-18 16:25:51 +0200

    added 3 inputs, name of the candidate, votes, add more candidate

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-# input field for class captain suggestions
+import random
+done_adding = False
+candidates, votes = [], []
 
-# Enter the total votes in the class
+while not done_adding:
 
-# Randomize the votes
+    # input field for class captain suggestions
+    name = str(input("Suggest a candidate for class president: "))
+    # Capitalize the input name 
+    candidates.append(name.capitalize())
 
-# Save the cast votes to file
+    # Enter how many votes that shoud be casted
+    nbr_votes = int(input("How many votes should the program randomize? "))
+
+    # Enter the total votes in the class
+    for candidate in candidates:
+        votes.append(nbr_votes)
+        print(candidate, votes)
+    
+    # Add the user for more suggestions
+    add_more = str(input("Add more candidate? Yes/No "))
+
+    # If no, randomize votes and save to file
+    if add_more.strip().lower() in ("no", "n"):
+        print("Inside no")
+        done_adding = True
+
+        # Save the cast votes to file
+
+
+    print(candidates)
